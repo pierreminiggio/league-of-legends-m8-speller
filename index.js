cfac8f05cba3dab018729260fd383e21b6521505
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-06-24 12:27:52 +0200

    L+key = cas on first ally

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ var robot = require("robotjs")
 
 const screenSize = robot.getScreenSize()
 
+let spell = null;
 ioHook.on('keydown', evt => {
     let key = String.fromCharCode(evt.rawcode)
+    if (['A', 'Z', 'E', 'R'].includes(key)) {
+        spell = key.toLowerCase()
+    }
     if (key === 'L') {
         console.log('go')
         let mousePos = robot.getMousePos()
 
         robot.moveMouse(screenSize.width - 150, screenSize.height - 380)
-        robot.mouseClick('left', false)
-        robot.keyTap('e')
+        robot.keyTap(spell)
 
         robot.moveMouse(mousePos.x, mousePos.y)
     }
