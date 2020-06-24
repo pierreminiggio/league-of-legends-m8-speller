1424e3c35d918010aad4d702dbe4f9f98a67840e
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-06-24 11:55:17 +0200

    need double taping e

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-// Move the mouse across the screen as a sine wave.
-var robot = require("robotjs");
- 
-const screenSize = robot.getScreenSize();
-console.log(screenSize)
+const ioHook = require('iohook')
+var robot = require("robotjs")
 
-robot.moveMouse(screenSize.width - 150, screenSize.height - 380);
-robot.mouseClick('left')
-robot.keyToggle('z', 'down')
+const screenSize = robot.getScreenSize()
+
+ioHook.on('keydown', evt => {
+    let key = String.fromCharCode(evt.rawcode)
+    if (key === 'L') {
+        console.log('go')
+        let mousePos = robot.getMousePos()
+
+        robot.moveMouse(screenSize.width - 150, screenSize.height - 380)
+        robot.mouseClick('left', false)
+        robot.keyTap('e')
+
+        robot.moveMouse(mousePos.x, mousePos.y)
+    }
+})
+
+ioHook.start()
